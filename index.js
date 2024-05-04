395de0f42ae78bb1b0b3cae24acc630c9be7c815
HaidarAli54 <haidarali5464@gmail.com>
2024-05-04 07:52:57 -0700

    membuat astroservices

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/',(req, res)) => {
+    res.send("Selamat datang di ASTROSERVICE");
+}
+
+app.get('/products',(req,res) => {
+    res.json({
+        
+    })
+})
